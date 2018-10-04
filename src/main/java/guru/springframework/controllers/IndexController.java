717c30947eeadd9cd146ca2154f40731b2a6b5d2
NachoPonisio <ponisio.ignacio@gmail.com>
2018-10-04 15:27:40 -0300

    added Spring Data JPA query methods

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,28 @@
 package guru.springframework.controllers;
 
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Autowired
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
-        System.out.println("asdaswasd");
+
+        System.out.println("Category Id is: "  + categoryRepository.findByDescription("Fast Food").get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasureRepository.findByDescription("Teaspoon").get().getId());
         return "index";
     }
 }
